46680d649ef56155709f738cf9f17ed3170c404c
Sebastian Felix Schwarz <schwarz@nexus-ion.ai>
2019-06-27 16:30:50 +0200

    init

--- handler.js
+++ handler.js
@@ -10,6 +10,11 @@ const ROOMS = process.env.ROOMS;
 module.exports.air = async (event, context) => {
   let room = ROOMS.split(",");
   let roomdata = [];
+
+  let lowest = Number.POSITIVE_INFINITY;
+  let highest = Number.NEGATIVE_INFINITY;
+  let tmp,coldroom,warmroom;
+
   for (let i=0;i<room.length;i++){
     let data = await s3.getObject({
       Bucket: BUCKET,
@@ -17,10 +22,35 @@ module.exports.air = async (event, context) => {
     }).promise();
     roomdata.push(JSON.parse(data.Body.toString()));
   }
+  console.log(roomdata);
+  for (let i=roomdata.length-1; i>=0; i--) {
+    tmp = roomdata[i].temp;
+    if (tmp < lowest){
+      lowest = tmp;
+      coldroom = roomdata[i].room;
+    }
+    if (tmp > highest){
+      highest = tmp;
+      warmroom = roomdata[i].room;
+    }
+  }
+  console.log(highest, lowest);
+  let obj = {
+    coldest: {
+      temp: lowest,
+      room: coldroom
+    },
+    warmest: {
+      temp: highest,
+      room: warmroom
+    }
+  };
+
+
 
   return {
     statusCode: 200,
-    body: roomdata
+    body: obj
     //body: JSON.parse(data.Body.toString())
   };
 };
